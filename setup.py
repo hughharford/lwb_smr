e2f6da901a25bec771e54eb59a78da027a3ccee4
Joshua Katzenberg <josh.katzenberg@gmail.com>
2022-06-08 10:27:19 +0100

    updating setup.py file to be able to install the package and run scripts correctly

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='lwb_solarmyroof',
+setup(name='lwb_smr',
       version="1.0",
       description="Project Description",
       packages=find_packages(),
@@ -13,5 +13,5 @@ setup(name='lwb_solarmyroof',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/lwb_solarmyroof-run'],
+      scripts=['scripts/lwb_smr-run'],
       zip_safe=False)
