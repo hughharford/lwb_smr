e4aa223270c4e9dc33afc6ccd720ba9eb06adfa0
hughharford <hugh.harford@poscoconsulting.com>
2022-06-09 16:19:52 +0100

    run via notebooks!

---
+++ tests/features/C_S/c001_add.feature
@@ -0,0 +1,7 @@
+Feature: CodeSignal c001 add
+    1st challenge
+
+    Scenario: Add two integers
+    Given integers a and b
+    When a and b are added
+    Then the result is a+b
---
+++ tests/step_defs/test_BDDstart_cucumbers_steps.py
@@ -0,0 +1,42 @@
+from pytest_bdd import scenarios, parsers, given, when, then
+
+from src.BDDstart.BBStart_cucumbers1 import CucumberBasket
+
+
+EXTRA_TYPES = {
+    'Number': int,
+}
+
+
+CONVERTERS = {
+    'initial': int,
+    'some': int,
+    'total': int,
+}
+
+
+scenarios('../features/BDDstart_cucumbers.feature')
+
+
+@given(parsers.cfparse('the basket has "{initial:Number}" cucumbers', extra_types=EXTRA_TYPES), target_fixture='basket')
+@given('the basket has "<initial>" cucumbers', target_fixture='basket')
+def basket(initial):
+    return CucumberBasket(initial_count=initial)
+
+
+@when(parsers.cfparse('"{some:Number}" cucumbers are added to the basket', extra_types=EXTRA_TYPES))
+@when('"<some>" cucumbers are added to the basket')
+def add_cucumbers(basket, some):
+    basket.add(some)
+
+
+@when(parsers.cfparse('"{some:Number}" cucumbers are removed from the basket', extra_types=EXTRA_TYPES))
+@when('"<some>" cucumbers are removed from the basket')
+def remove_cucumbers(basket, some):
+    basket.remove(some)
+
+
+@then(parsers.cfparse('the basket contains "{total:Number}" cucumbers', extra_types=EXTRA_TYPES))
+@then('the basket contains "<total>" cucumbers')
+def basket_has_total(basket, total):
+    assert basket.count == total
