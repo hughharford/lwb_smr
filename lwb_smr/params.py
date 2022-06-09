780f0281a21461b6adbe7d04ca50cf9eecaf305e
Jack Housego <jack.housego@btinternet.com>
2022-06-09 16:33:21 +0100

    added params.py for constant params to be used, added test_trainer.py to test pipeline, added utils_class.mlflowpush.py stores MLFLow class in its own file. I would like to remove utils.py and store utility functions/classes in a utils folder.

--- lwb_smr/params.py
+++ lwb_smr/params.py
@@ -2,3 +2,17 @@
 File to store parameters for reuse
 
 """
+
+# MLFlow URI to save model parameters and losses
+MLFLOW_URI = "https://mlflow.lewagon.ai/"
+
+
+# Dictionary containing paths to VM_folders for training, validation and test data
+VM_path_dict = {
+    'path_X_train': '../raw_data/AerialImageDataset/train/images/',
+    'path_y_train': '../raw_data/AerialImageDataset/train/gt/',
+    'path_X_val': '',
+    'path_y_val': '',
+    'path_X_test': '',
+    'path_y_test': ''
+}
